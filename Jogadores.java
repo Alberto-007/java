73c8cf50e79dec5f1388c7e8935ad11cc569bf21
Alberto <139139078+Alberto-007@users.noreply.github.com>
2024-02-21 15:11:49 -0300

    Create Jogadores.java

---
+++ Jogadores.java
@@ -0,0 +1,22 @@
+class Jogadores {
+    public String getNome() {
+		return nome;
+	}
+
+	public void setNome(final String nome) {
+		this.nome = nome;
+	}
+
+	String nome;
+
+}
+
+class Jogador2 {
+    String nome;
+
+}
+
+class Jogador3 {
+    String nome;
+
+}
